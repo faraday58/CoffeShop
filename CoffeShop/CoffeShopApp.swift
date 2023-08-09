37ed2d6dc21b08308c74edabb950cc44851eedb8
Armando <jarodriguezvera@gmail.com>
2023-08-09 12:44:28 -0600

    Initial Commit

---
+++ CoffeShop/CoffeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeShopApp.swift
+//  CoffeShop
+//
+//  Created by Armando Rodríguez on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
